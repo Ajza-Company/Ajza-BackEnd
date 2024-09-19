eea8c4ccc5a3773d62103852c2d6153f639652df
Khaled EL-Azab <shw2ypro@gmail.com>
2024-09-19 21:04:40 +0300

    ✨ feat: register TelescopeServiceProvider conditionally in AppServiceProvider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\F_RepositoryServiceProvider::class,
     App\Providers\HelperServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
     Spatie\Permission\PermissionServiceProvider::class,
 ];
