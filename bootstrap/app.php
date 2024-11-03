6682b8bae299b5725bd2d7fe28394edc2220d7aa
Khaled EL-Azab <shw2ypro@gmail.com>
2024-11-03 15:01:46 +0200

    🚮 `delete`: Remove unused `Authenticate` middleware and clean up related code in `app.php` and `frontend.php`

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\Authenticate;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -17,7 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         },
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(Authenticate::class);
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
