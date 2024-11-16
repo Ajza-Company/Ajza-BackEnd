858893b54fa89835d746d23dc3d492afe989bd6e
Khaled EL-Azab <shw2ypro@gmail.com>
2024-11-17 00:34:06 +0200

    🎉 feat(bootstrap): add Echo API for real-time event broadcasting in Laravel applications
    🔧 chore(composer): add laravel/reverb package for WebSocket communication
    
    🔒 chore(composer.lock): update content hash after adding new dependencies
    
    📦 fix(package): update plugin API version in composer.json to 2.6.0

---
+++ app/Http/Controllers/api/v1/Frontend/F_RepOrderController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\api\v1\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class F_RepOrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function createOrder()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function orders(Request $request)
+    {
+        //
+    }
+}
