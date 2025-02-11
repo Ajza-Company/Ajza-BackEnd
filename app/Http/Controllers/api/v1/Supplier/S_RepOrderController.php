4925f23a864f09a76161b51fca5709229bac6f15
Khaled EL-Azab <shw2ypro@gmail.com>
2025-02-11 23:41:12 +0200

    🔧 fix(S_RepOrderController.php): correct user ID filter in allOrders method for accurate data retrieval

--- app/Http/Controllers/api/v1/Supplier/S_RepOrderController.php
+++ app/Http/Controllers/api/v1/Supplier/S_RepOrderController.php
@@ -43,6 +43,6 @@ class S_RepOrderController extends Controller
      */
     public function allOrders()
     {
-        return S_ShortRepOrderResource::collection(RepOrder::whereRelation('repChats', 'user2_id', auth('api')->id())->adaptivePaginate());
+        return S_ShortRepOrderResource::collection(RepOrder::whereRelation('repChats', 'user1_id', auth('api')->id())->adaptivePaginate());
     }
 }
