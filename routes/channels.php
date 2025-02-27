b2b075b496dcd172443bce72d88501630b463264
Khaled EL-Azab <shw2ypro@gmail.com>
2025-02-27 21:23:07 +0200

    🔧 fix(routes/channels.php): log user ID and chat ID for repair chat channel authorization check

--- routes/channels.php
+++ routes/channels.php
@@ -6,8 +6,9 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
 Broadcast::channel('repair.chat.{chatId}', function ($user, $chatId) {
-    return auth()->check();
-    /*$chat = RepChat::findOrFail($chatId);
-    return $user->id === $chat->user1_id || $user->id === $chat->user2_id;*/
+    Log::info('channel: ' . $user->id . ' ' . $chatId);
+    $chat = RepChat::findOrFail($chatId);
+    return $user->id === $chat->user1_id || $user->id === $chat->user2_id;
 });
