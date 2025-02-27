7acc807e9b5e362b2f4162128bdd12907394d748
Khaled EL-Azab <shw2ypro@gmail.com>
2025-02-27 21:10:05 +0200

    🧹 refactor(routes): remove unused private channel and add repair chat channel for user access control

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Models\RepChat;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+Broadcast::channel('repair.chat.{chatId}', function ($user, $chatId) {
+    $chat = RepChat::findOrFail($chatId);
+    return $user->id === $chat->user1_id || $user->id === $chat->user2_id;
 });
--- routes/console.php
+++ routes/console.php
@@ -7,8 +7,3 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
-
-Broadcast::channel('private-repair.chat.{chatId}', function ($user, $chatId) {
-    $chat = RepChat::findOrFail($chatId);
-    return $user->id === $chat->user1_id || $user->id === $chat->user2_id;
-});
