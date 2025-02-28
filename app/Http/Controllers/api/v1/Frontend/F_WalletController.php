18ea39203f37a8ab251ebc5a06f8f318193fdcaa
Khaled EL-Azab <shw2ypro@gmail.com>
2025-02-28 03:07:44 +0200

    ✨ feat(api): create wallet for user if it doesn't exist in F_WalletController

--- app/Http/Controllers/api/v1/Frontend/F_WalletController.php
+++ app/Http/Controllers/api/v1/Frontend/F_WalletController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api\v1\Frontend;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\v1\Frontend\Wallet\F_ShortWalletResource;
 use App\Http\Resources\v1\Frontend\Wallet\F_WalletResource;
+use App\Models\Wallet;
 use Illuminate\Http\Request;
 
 class F_WalletController extends Controller
@@ -14,6 +15,11 @@ class F_WalletController extends Controller
      */
     public function __invoke()
     {
+        if (auth('api')->user()->wallet == null) {
+            Wallet::create([
+                'user_id' => auth('api')->id()
+            ]);
+        }
         return F_ShortWalletResource::collection(auth('api')->user()->wallet?->transactions()?->latest()->adaptivePaginate())->additional([
             'balance' => auth('api')->user()->wallet?->balance
         ]);
