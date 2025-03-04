cde781d14e6458eea075cab7998498939b37ce0f
omar hamdy <hamdyomar065@gmail.com>
2025-03-04 22:33:19 +0200

    create store product

--- app/Http/Controllers/api/v1/Supplier/S_ProductController.php
+++ app/Http/Controllers/api/v1/Supplier/S_ProductController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\api\v1\Supplier;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\v1\Supplier\Product\StoreProductRequest;
+use App\Repositories\Supplier\Store\Find\S_FindStoreInterface;
 use App\Http\Resources\v1\Supplier\Product\S_ShortProductResource;
 use App\Http\Resources\v1\Supplier\StoreProduct\S_ShortStoreProductResource;
-use App\Repositories\Supplier\Store\Find\S_FindStoreInterface;
-use Illuminate\Http\Request;
+use App\Services\Supplier\Product\S_CreateProductService;
 
 class S_ProductController extends Controller
 {
@@ -15,7 +17,8 @@ class S_ProductController extends Controller
      *
      * @param S_FindStoreInterface $findStore
      */
-    public function __construct(private S_FindStoreInterface $findStore)
+    public function __construct(private S_FindStoreInterface $findStore,
+                                private S_CreateProductService $createProduct)
     {
 
     }
@@ -38,9 +41,9 @@ class S_ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreProductRequest $request)
     {
-        //
+        return $this->createProduct->create($request->validated());
     }
 
     /**
