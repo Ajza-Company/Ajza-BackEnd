322237d435f4c03771d75f87294cc327201e86d1
omar hamdy <hamdyomar065@gmail.com>
2025-03-19 00:32:25 +0200

    update image

--- app/Http/Requests/v1/Admin/Product/F_UpdateProductRequest.php
+++ app/Http/Requests/v1/Admin/Product/F_UpdateProductRequest.php
@@ -33,7 +33,7 @@ class F_UpdateProductRequest extends FormRequest
             'localized.*.description' => 'required|string',
             'price' => 'required|numeric|min:0',
             'part_number' => 'nullable|string|max:50',
-            'image' => 'nullable|string|max:255',
+            'image' => 'sometime|nullable|file|max:10240',
             'variate' => 'required|array|min:1',
             'variate.*.variant_category_id' => 'required|integer|exists:variant_categories,id',
             'variate.*.value' => 'required|string|max:100'
