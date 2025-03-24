afdbc13a5485a5577f8007a1a61c79d01ea88a96
Khaled EL-Azab <shw2ypro@gmail.com>
2025-03-25 00:30:07 +0200

    🔧 fix(app/Listeners/SendOrderCreatedNotification.php): filter users by FCM tokens for order notifications

--- app/Listeners/SendOrderCreatedNotification.php
+++ app/Listeners/SendOrderCreatedNotification.php
@@ -24,7 +24,7 @@ class SendOrderCreatedNotification implements ShouldQueue
     public function handle(F_OrderCreatedEvent $event): void
     {
         \Log::info('SendOrderCreatedNotification' . json_encode($event->order->store->users()->get()));
-        Notification::send($event->order->store->users()->get(), new OrderNotification(
+        Notification::send($event->order->store->users()->whereHas('userFcmTokens')->get(), new OrderNotification(
             order: $event->order,
             type: 'order_created'
         ));
