127d59a0ceb12b11463dffb4088590caed9d818e
Khaled EL-Azab <shw2ypro@gmail.com>
2025-03-26 04:03:01 +0200

    🔧 fix(app): update address resource to use F_AddressResource for better data handling in S_ShortRepOrderResource

--- app/Http/Resources/v1/Supplier/RepOrder/S_ShortRepOrderResource.php
+++ app/Http/Resources/v1/Supplier/RepOrder/S_ShortRepOrderResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\v1\Supplier\RepOrder;
 
+use App\Http\Resources\v1\Frontend\Address\F_AddressResource;
 use App\Http\Resources\v1\Frontend\Address\F_ShortAddressResource;
 use App\Http\Resources\v1\General\RepChat\G_RepChatResource;
 use Illuminate\Http\Request;
@@ -23,7 +24,7 @@ class S_ShortRepOrderResource extends JsonResource
             'image' => getFullUrl($this->image),
             'status' => $this->status,
             'created_at' => $this->created_at,
-            'address' => $this->whenLoaded('address', new F_ShortAddressResource($this->address)),
+            'address' => $this->whenLoaded('address', new F_AddressResource($this->address)),
             'chat' => $this->whenLoaded('repChat', new G_RepChatResource($this->repChat)),
         ];
     }
