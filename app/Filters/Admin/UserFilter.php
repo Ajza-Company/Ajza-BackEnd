a183bdaf809e329a84e2739833780c7973591498
omar hamdy <56228592+omarhamdy010@users.noreply.github.com>
2025-05-07 00:29:37 +0300

    active user

--- app/Filters/Admin/UserFilter.php
+++ app/Filters/Admin/UserFilter.php
@@ -4,12 +4,14 @@ namespace App\Filters\Admin;
 
 use App\Filters\Admin\Filters\User\GeneralFilter;
 use App\Filters\Admin\Filters\User\StatusFilter;
+use App\Filters\Admin\Filters\User\ActiveFilter;
 use App\Filters\FilterClass;
 
 class UserFilter extends FilterClass
 {
     protected array $filters = [
         'search' => GeneralFilter::class,
-        'status' => StatusFilter::class
+        'status' => StatusFilter::class,
+        'active' => ActiveFilter::class
     ];
 }
