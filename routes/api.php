9cda60ca1a8fe70e73d108d9d72f32bf46623cca
Khaled EL-Azab <shw2ypro@gmail.com>
2025-05-15 18:54:29 +0300

    🔄 update(routes/api.php): change payment callback route from GET to POST method

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\api\v1\Frontend\F_PaymentCallbackController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('payment/callback', F_PaymentCallbackController::class)->name('payment.callback');
+Route::post('payment/callback', F_PaymentCallbackController::class)->name('payment.callback');
