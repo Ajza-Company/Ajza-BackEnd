906f05eb651a8c51dafe8e1de957dccefc7a44bc
Khaled EL-Azab <shw2ypro@gmail.com>
2025-05-29 20:01:56 +0300

    🚀 refactor(F_SliderImageResource.php): remove debug statement and return media URL in array format

--- app/Http/Resources/v1/Frontend/SliderImage/F_SliderImageResource.php
+++ app/Http/Resources/v1/Frontend/SliderImage/F_SliderImageResource.php
@@ -14,7 +14,6 @@ class F_SliderImageResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        dd($this->getFirstMediaUrl('sliders'));
         return [
             'id' => encodeString($this->id),
             'image' => $this->getFirstMediaUrl('sliders'),
